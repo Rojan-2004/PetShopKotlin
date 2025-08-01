18c145d2b8ed02684a483e9ce56ff6cbc4919dcd
Rojan-2004 <rojanm1000@gmail.com>
2025-08-01 13:18:10 +0545

    userview

--- app/src/main/java/com/example/petshop/viewmodel/UserViewModelFactory.kt
+++ app/src/main/java/com/example/petshop/viewmodel/UserViewModelFactory.kt
@@ -1,4 +1,16 @@
 package com.example.petshop.viewmodel
 
-class UserViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.petshop.repository.UserRepository
+
+class UserViewModelFactory(
+    private val repository: UserRepository
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(UserViewModel::class.java)) {
+            return UserViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
