b24c621585ed54567046fb48a66cfb4f5b403e11
Rojan-2004 <rojanm1000@gmail.com>
2025-08-01 11:08:45 +0545

    model

---
+++ app/src/main/java/com/example/petshop/model/UserModel.kt
@@ -0,0 +1,11 @@
+package com.example.petshop.model
+
+data class UserModel(
+    var userId : String = " ",
+    var email : String = " ",
+    var firstName : String = " ",
+    var gender : String = " ",
+    var address:  String = " ",
+    var number: String = " ",
+    var image  : String = ""
+)
---
+++ app/src/main/java/com/example/petshop/viewmodel/UserViewModelFactory.kt
@@ -0,0 +1,4 @@
+package com.example.petshop.viewmodel
+
+class UserViewModelFactory {
+}
